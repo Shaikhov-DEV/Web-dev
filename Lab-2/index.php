d3c2cd876487876121522bc6bf71515dac47e6b1
Shaikhov-DEV <81424149+Shaikhov-DEV@users.noreply.github.com>
2021-11-29 02:31:44 +0500

    Move index

--- Lab-2/index.php
+++ Lab-2/index.php
@@ -2,9 +2,9 @@
 	<p> Лабораторная работа №2 <br>
            ?>
 <u1>
-        <li><a href="http://php-shaikhov/Lab-2/lab-2-1.php">Задание 2-1</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-2-2.php">Задание 2-2</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-2-3.php">Задание 2-3</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-2-4.php">Сам. работа - задача 16</a></li>
-        <li><a href="http://php-shaikhov/Lab-2/lab-2-5.php">Сам. работа - задача 5</a></li>
+        <li><a href="http://http://z0599309.xsph.ru/lab-2-1.php">Задание 2-1</a></li>
+		<li><a href="http://http://z0599309.xsph.ru/lab-2-2.php">Задание 2-2</a></li>
+		<li><a href="http://http://z0599309.xsph.ru/lab-2-3.php">Задание 2-3</a></li>
+		<li><a href="http://http://z0599309.xsph.ru/lab-2-4.php">Сам. работа - задача 16</a></li>
+        <li><a href="http://http://z0599309.xsph.ru/lab-2-5.php">Сам. работа - задача 5</a></li>
 </u1>
