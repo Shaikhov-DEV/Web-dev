43fc529657b69dc9234cac3ce84df32067efe961
Shaikhov-DEV <81424149+Shaikhov-DEV@users.noreply.github.com>
2021-11-29 02:07:51 +0500

    Edit index for lab#2

--- Lab-2/index.php
+++ Lab-2/index.php
@@ -1,26 +1,10 @@
 <p> Шаихов Х.И. ПИ-323 <br>
-	<p> Дата и время:
-          <?php
-           $d=date("d.m.Y H:i");
-             echo($d);
+	<p> Лабораторная работа №2 <br>
            ?>
 <u1>
-        <li><a href="http://php-shaikhov/Lab-2/lab-1-1.php">lab-1-1</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-2.php">lab-1-2</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-3.php">lab-1-3</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-4.php">lab-1-4</a></li>
-        <li><a href="http://php-shaikhov/Lab-2/lab-1-5.php">lab-1-5</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-6.php">lab-1-6</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-7.php">lab-1-7</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-8.php">lab-1-8</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-9.php">lab-1-9</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-10.php">lab-1-10</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-11.php">lab-1-11</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-12.php">lab-1-12</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-13.php">lab-1-13</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-14.php">lab-1-14</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-15.php">lab-1-15</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-16.php">lab-1-16</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-1-17.php">lab-1-17</a></li>
-		<li><a href="http://php-shaikhov/Lab-2/lab-2-1.php">lab-2-1</a></li>	
+        <li><a href="http://php-shaikhov/Lab-2/lab-2-1.php">Задание 2-1</a></li>
+		<li><a href="http://php-shaikhov/Lab-2/lab-2-2.php">Задание 2-2</a></li>
+		<li><a href="http://php-shaikhov/Lab-2/lab-2-3.php">Задание 2-3</a></li>
+		<li><a href="http://php-shaikhov/Lab-2/lab-2-4.php">Сам. работа - задача 16</a></li>
+        <li><a href="http://php-shaikhov/Lab-2/lab-2-5.php">Сам. работа - задача 5</a></li>
 </u1>
