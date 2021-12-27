ab7f4117a1ae2319a040dc3a82bd273311a5bca3
Shaikhov-DEV <khalil.shaikhov@mail.ru>
2021-12-28 04:33:54 +0500

    Edit LB4 for LB5
    Edit LB4 for LB5

---
+++ Lab-4/keyAdm.php
@@ -0,0 +1,70 @@
+<html>
+<head><title> Сведения о ключах </title></head>
+<body  align="center" background="/5WWU.gif">
+<h2>Страница администратора</h2>
+<h2>Шаихов Халил ПИ-323</h2>
+<h2>Сведения о ключах:</h2>
+<table align="center" border="1">
+    <tr>
+        <th>ID ключа</th>
+        <th>дата приобретения</th>
+        <th> дата окончания</th>
+        <th> ОС</th>
+        <th> магазин</th>
+        <th> стоимость</th>
+        <th> ключ</th>
+        <th> Редактировать</th>
+        <th> Уничтожить</th>
+    </tr>
+    </tr>
+    <?php
+    include("checks.php");
+    require_once 'connect.php';
+    $mysqli = new mysqli($host, $user, $password, $database);
+    if ($mysqli->connect_errno) {
+        echo "Невозможно подключиться к серверу";
+    }
+   $result=$mysqli->query("SELECT kl.id_key, kl.key_date, 
+   kl.key_date_end, os.os_name as os, stores.stores_name as stores, 
+   kl.key_cost, kl.key_name
+FROM kl 
+LEFT JOIN os ON kl.id_os=os.id_os
+LEFT JOIN stores ON kl.id_stores=stores.id_stores"); // запрос на
+
+
+    $counter = 0;
+     if ($result) {
+        while ($row = $result->fetch_array()) {// для каждой строки из запроса
+            $id = $row['id_key'];
+            $date_buy = $row['key_date'];
+            $date_ex = $row['key_date_end'];
+            $os = $row['os'];
+            $ds = $row['stores'];
+            $price = $row['key_cost'];
+            $key_os = $row['key_name'];
+
+            $date_buy = date('d-m-Y', strtotime($date_buy));
+            $date_ex = date('d-m-Y', strtotime($date_ex));
+
+            echo "<tr>";
+            echo "<td>$id</td><td>$date_buy</td><td>$date_ex</td><td>$os</td><td>$ds</td><td>$price</td><td>$key_os</td>";
+
+            echo "<td><a href='edit_key.php?id_key=" . $row['id_key']
+                . "'>Редактировать</a></td>"; //Запуск редактирования
+            echo "<td><a href='delete_key.php?id_key=" . $row['id_key']
+                . "'>Удалить</a></td>"; //запуск удаления
+            echo "</tr>";
+            $counter++;
+        }
+    }
+    print "</table>";
+    print("<p>Всего ОС: $counter </p>");
+    echo "<p><a href=new_key.php> Добавить ключ </a>";
+    if ($_SESSION['type'] == 1)
+        echo "<p><a href=os.php> Вернуться назад </a>";
+    elseif ($_SESSION['type'] == 2)
+        echo "<p><a href=osAdm.php> Вернуться назад </a>";
+    include("checkSession.php");
+    ?>
+</body>
+</html>
---
+++ Lab-4/storesAdm.php
@@ -0,0 +1,46 @@
+<html>
+<head><title> Сведения о магазинах </title></head>
+<body  align="center" background="/5WWU.gif">
+<h2>Страница администратора</h2>
+<h2>Шаихов Халил ПИ-323</h2>
+<h2>Сведения о магазинах:</h2>
+<table align="center" border="1">
+    <tr>
+        <th>id магазина</th>
+        <th>название</th>
+        <th>url</th>
+        <th>Редактировать</th>
+        <th>Уничтожить</th>
+    </tr>
+    </tr>
+    <?php
+    include("checks.php");
+    require_once 'connect.php';
+    $link = mysqli_connect($host, $user, $password, $database) or die ("Невозможно
+подключиться к серверу" . mysqli_error($link));
+$result=mysqli_query($link, "SELECT id_stores, stores_name, stores_url FROM stores"); // запрос на выборку сведений о пользователях
+    mysqli_select_db($link, "stores");
+
+while ($row=mysqli_fetch_array($result)){// для каждой строки из запроса
+ echo "<tr>";
+ echo "<td>" . $row['id_stores'] . "</td>";
+ echo "<td>" . $row['stores_name'] . "</td>";
+ echo "<td>" . $row['stores_url'] . "</td>";
+        echo "<td><a href='edit_store.php?id_stores=" . $row['id_stores']
+            . "'>Редактировать</a></td>"; // запуск скрипта для редактирования
+        echo "<td><a href='delete_store.php?id_stores=" . $row['id_stores']
+            . "'>Удалить</a></td>"; // запуск скрипта для удаления записи
+        echo "</tr>";
+    }
+    print "</table>";
+    $num_rows = mysqli_num_rows($result); // число записей в таблице БД
+    print("<P>Всего магазинов: $num_rows </p>");
+    echo "<p><a href=new_store.php> Добавить магазин </a>";
+    if ($_SESSION['type'] == 1)
+        echo "<p><a href=os.php> Вернуться назад </a>";
+    elseif ($_SESSION['type'] == 2)
+        echo "<p><a href=osAdm.php> Вернуться назад </a>";
+    include("checkSession.php");
+    ?>
+</body>
+</html>
